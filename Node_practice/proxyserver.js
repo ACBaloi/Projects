75d7eb025e29af0e3d531795193bb8c8440c604c
ACBaloi <anacajingue@gmail.com>
2024-05-10 15:21:59 +0200

    Build a Node.js HTTP Proxy Server

---
+++ Node_practice/proxyserver.js
@@ -0,0 +1,13 @@
+let http = require('http');
+let proxy = require ('http-proxy');
+
+proxyServer = proxy.createProxyServer({ target: 'http://127.0.0.1:9000'});
+proxyServer.listen(8000);
+
+server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Proxy Request was Successful' + '\n' + JSON.stringify(req.headers, true, 2));
+    res.end();
+});
+
+server.listen(9000);
