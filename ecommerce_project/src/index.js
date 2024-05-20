00bf54ede708cc0038b2514b29f52819ccba076f
ACBaloi <anacajingue@gmail.com>
2024-05-20 14:43:44 +0200

    Implemented working routes

--- ecommerce_project/src/index.js
+++ ecommerce_project/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import ProductList from './Components/ProductList';
+import Default from './Components/Default';
+
+const router = createBrowserRouter([
+  {
+      path:"/",
+      element: <ProductList />,
+      errorElement: <Default />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
