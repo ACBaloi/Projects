d7383b93f749edf89de5e5e4a918df417120a35a
ACBaloi <anacajingue@gmail.com>
2024-08-09 18:14:34 +0200

    Making navbar responsive

--- Portfolio-Page/script.js
+++ Portfolio-Page/script.js
@@ -1,5 +1,12 @@
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Mensagem enviada com sucesso!');
-})
+});
 
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const nav = document.getElementsByClassName('nav')[0]
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
