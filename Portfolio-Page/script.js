8eece38385cc4b67715272a708e92a4895241bd3
ACBaloi <anacajingue@gmail.com>
2024-08-11 06:49:15 +0200

    Added media queries to pages

--- Portfolio-Page/script.js
+++ Portfolio-Page/script.js
@@ -1,12 +1,15 @@
+//contacto.html
+
 document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Mensagem enviada com sucesso!');
 });
 
+//index.html
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const nav = document.getElementsByClassName('nav')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    nav.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
 })
